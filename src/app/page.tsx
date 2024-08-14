606ba10f1788054417ac915e19a0580f400ffcfb
sujang958 <sujangdev@gmail.com>
2024-08-14 22:57:00 +0900

    feat: add a heart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Container from "@/components/Container";
+import WritingItem from "@/components/WritingItem";
 import { Search } from "lucide-react";
 import Image from "next/image";
 
@@ -9,14 +10,12 @@ export default function Home() {
 
       <section className="mt-8">
         <p className="text-xl font-semibold">인기있는 글</p>
-        <div className="flex flex-row items-center justify-start mt-4 gap-x-4">
-          <div className="cursor-pointer">
-            <Image src="/cover.png" width={1024 * 0.14} height={1401 * 0.14} alt="cover" className="object-contain rounded-lg" draggable={false} />
+        <div className="flex flex-row items-center justify-start mt-4 gap-x-8">
+          <WritingItem />
+          <WritingItem />
+          <WritingItem />
+          <WritingItem />
 
-            <p className="text-sm text-neutral-400 mt-3.5">에로티시즘</p>
-            <p className="text-lg font-semibold">롤리타</p>
-            <p className="text-sm text-neutral-400 mt-0.5">블라디미르 지음</p>
-          </div>
         </div>
       </section>
 
