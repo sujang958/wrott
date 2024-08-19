6017cac77bd747513ba801f6c63307d2bcc146e7
sujang958 <sujangdev@gmail.com>
2024-08-20 00:01:42 +0900

    feat: add gluestack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,39 @@
-import { Feather } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
-import { ComponentProps, FC } from "react";
-import { View } from "react-native-reanimated/lib/typescript/Animated";
+import { Feather, Ionicons } from "@expo/vector-icons"
+import { Tabs } from "expo-router"
+import { ComponentProps, FC } from "react"
+import { View } from "react-native-reanimated/lib/typescript/Animated"
 
 const RouteIcons = {
-  index: (props: Omit<ComponentProps<typeof Feather>, 'name'>) => <Feather {...props} name="home" />,
-  search: (props: Omit<ComponentProps<typeof Feather>, 'name'>) => <Feather {...props} name="search" />,
+  index: (props: Omit<ComponentProps<typeof Feather>, "name">) => (
+    <Feather {...props} name="home" />
+  ),
+  search: (props: Omit<ComponentProps<typeof Feather>, "name">) => (
+    <Feather {...props} name="search" />
+  ),
+  library: (props: Omit<ComponentProps<typeof Feather>, "name">) => (
+    <Ionicons {...props} name="library-outline" />
+  ),
 } as const
 
 export default function RootLayout() {
   return (
-    <Tabs screenOptions={ ({route}) => ({
-      header: undefined,
-      tabBarIcon: ({ focused, color, size }) => {
-        const Icon = RouteIcons[route.name as keyof typeof RouteIcons]
-
-        // You can return any component that you like here!
-        return <Icon color={color} size={size} />
-      },
-      tabBarActiveTintColor: 'tomato',
-      tabBarInactiveTintColor: 'gray',
-    })}>
+    <Tabs
+      sceneContainerStyle={{ maxWidth: 1200 }}
+      screenOptions={({ route }) => ({
+        tabBarShowLabel: true,
+        headerShown: false,
+        tabBarStyle: {},
+        tabBarIcon: ({ focused, color, size }) => {
+          const Icon = RouteIcons[route.name as keyof typeof RouteIcons]
+          return <Icon color={color} size={size} />
+        },
+        tabBarActiveTintColor: "blue",
+        tabBarIconStyle: { flexDirection: "column" },
+      })}
+    >
       <Tabs.Screen name="index" />
       <Tabs.Screen name="search" />
+      <Tabs.Screen name="library" />
     </Tabs>
-  );
+  )
 }
---
+++ app/library.tsx
@@ -0,0 +1,14 @@
+import { Button, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+      }}
+    >
+      <Text>asdfasfasf.</Text>
+    </SafeAreaView>
+  );
+}
