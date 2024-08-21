4fdbf5ce9eca98c5dc6ff676a7c859d1295e2156
sujang958 <enter7377@naver.com>
2024-08-21 22:19:04 +0900

    feat: i wanna kms expo is so dumb

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,70 @@
 import { Feather } from "@expo/vector-icons"
 import { Tabs } from "expo-router"
 import React from "react"
+import { Text } from "react-native"
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "blue",
+        tabBarActiveTintColor: "#1d4ed8",
         headerShown: false,
+
+        tabBarStyle: {
+          bottom: 24,
+          maxWidth: 500,
+          borderRadius: 16,
+          alignSelf: "center",
+          width: "90%",
+          borderWidth: 0,
+          //paddingVertical: 12,
+          // flexDirection: "row",
+          // alignItems: "center",
+          // alignContent: "center",
+          // justifyContent: "space-between",
+        },
+        tabBarLabelPosition: "below-icon",
+        tabBarShowLabel: false,
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
+          tabBarLabel: ({ color, focused }) => {
+            return (
+              <Text style={{ color, fontSize: 12, marginTop: -8 }}>홈</Text>
+            )
+          },
           tabBarIcon: ({ color, focused }) => (
-            <Feather name="home" color={color} />
+            <Feather name="home" size={20} color={color} />
           ),
         }}
       />
       <Tabs.Screen
         name="search"
         options={{
+          tabBarLabel: ({ color, focused }) => {
+            return (
+              <Text style={{ color, fontSize: 12, marginTop: -8 }}>검색</Text>
+            )
+          },
           tabBarIcon: ({ color, focused }) => (
-            <Feather name="search" color={color} />
+            <Feather name="search" size={18} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="library"
+        options={{
+          tabBarLabel: ({ color, focused }) => {
+            return (
+              <Text style={{ color, fontSize: 12, marginTop: -8 }}>
+                내 서재
+              </Text>
+            )
+          },
+          tabBarIcon: ({ color, focused }) => (
+            <Feather name="columns" size={18} color={color} />
           ),
         }}
       />
